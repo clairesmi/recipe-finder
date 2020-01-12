8bd77ed99a90590f787a7d27117e55523679c75e
Claire <smith.clairelouise@gmail.com>
2020-01-12 22:53:40 +0000

    trial version ready

--- recipe-finder/src/App.js
+++ recipe-finder/src/App.js
@@ -25,13 +25,13 @@ class App extends Component {
   }
 
   // REMOVE AFTER TESTING
-  // componentDidMount = async () => {
-  //   // console.log(ingredients)
-  //   const res = await axios.post('https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?q=tomatoes')
-  //   console.log(res.data.results)
-  //   this.setState({ recipes: res.data.results })
-  //   this.toggleLoading()
-  // }
+  componentDidMount = async () => {
+    // console.log(ingredients)
+    const res = await axios.post('https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?q=tomatoes')
+    console.log(res.data.results)
+    this.setState({ recipes: res.data.results })
+    // this.toggleLoading()
+  }
 
   toggleListen = async () => {
     await this.setState({ listening: !this.state.listening })
@@ -125,6 +125,8 @@ class App extends Component {
             console.error(e)
           }
     }
+    
+    // if (recipes.length)
     // if (ingredients.length === 4) {
     //   ingredients = this.state.finalTranscript.split(' ')
     //   try {
@@ -135,10 +137,9 @@ class App extends Component {
     //         console.error(e)
     //       }
     // }
-    else if (!this.state.recipes) {
+    else if (!this.state.recipes || this.state.recipes.length === 0) {
       await this.setState({requestError: true})
-      console.log(this.state.requestError) // use this state to put an error message up for the user (alert)
-      console.log(this.state.recipes)
+      this.setState({recipes: null})
       }
   }
 
@@ -152,6 +153,7 @@ class App extends Component {
     this.setState({recipes: null})
     this.setState({finalTranscript: ''})
     this.setState({requestError: false})
+    this.setState({toggleRecipeDisplay: false})
     // this.toggleLoading()
   }
 
@@ -163,8 +165,10 @@ class App extends Component {
     console.log(!this.state.requestError) 
     const { finalTranscript, recipes, toggleRecipeDisplay, requestError} = this.state
     const firstFiveRecipes = recipes ? recipes.slice(0, 5) : null
+    const lastFiveRecipes = recipes ? recipes.slice(5, 10) : null
     return ( 
       <div className='main-wrapper' style={main}>
+        {!recipes &&
         <div style={!requestError ? pageWrapper: outerModal}>
           <div className='header' style={header}>
             <h1 style={headerText}>Talk Foodie to Me</h1>
@@ -183,54 +187,60 @@ class App extends Component {
       {/* <span>{this.transcript}</span> */}
         <button id='microphone-btn' style={buttonOne} onClick={this.toggleListen}>SAY INGREDIENTS</button>
         <button id='microphone-btn' style={buttonTwo} onClick={this.stopToggleListen}>FIND RECIPES</button>
-        <button onClick={this.resetTranscript} style={buttonThree}>START AGAIN</button>
+        <button onClick={this.resetTranscript} style={buttonThree}>FIND MORE RECIPES</button>
         {/* <div id='interim' style={interim}>{interimTranscript} </div> */}
         </div>
         <div style={transcriptLabel}>You asked for recipes for:</div>
         <div id='final' style={final}>{finalTranscript}</div>
         
       </div>
-      <>
+      </div>
+      }
+      {recipes &&
+      <div style={recipeWrapper} id='recipes' className='recipes'>
+      
       <div style={recipeShow}>
       {recipes &&
-      <div className="five-recipes">
-      Recipes
+      <div style={recipeList} className="five-recipes">
+      <button onClick={this.resetTranscript} style={buttonFour}>Go Back</button>
+        <h1 style={recipeHeader}>
+      Recipes</h1>
+      <div className='underline' style={underLine}></div>
       {firstFiveRecipes.map((recipe, i) => (
         <>
-              <label key={i}>
-                <a href={recipe.href} rel="noopener noreferrer" target="_blank">
+              {/* <label key={i}> */}
+                <a key={i} href={recipe.href} rel="noopener noreferrer" target="_blank">
                   <h3>{recipe.title}</h3>
                   <p>{recipe.ingredients}</p>
                 </a> 
-              </label> 
+              {/* </label>  */}
             </>
             ))}
-            {!toggleRecipeDisplay && <button onClick={this.toggleRecipeDisplay}>Show me more</button>}
+            {!toggleRecipeDisplay && <button onClick={this.toggleRecipeDisplay} style={buttonFour}>Show me more</button>}
             </div>
             }
 
             {recipes && toggleRecipeDisplay &&
-            <div className="ten-recipes">
-              {recipes.map((recipe, i) => (
+            <div style={recipeList} className="ten-recipes">
+              {lastFiveRecipes.map((recipe, i) => (
                 <>
-              <label key={i}>
-                <a href={recipe.href} rel="noopener noreferrer" target="_blank">
+                <a key={i} href={recipe.href} rel="noopener noreferrer" target="_blank">
                   <h3>{recipe.title}</h3>
                   <p>{recipe.ingredients}</p>
                 </a> 
-              </label> 
               </>
             ))}
-            <button onClick={this.toggleRecipeDisplay}>Show less</button>
+            <button onClick={this.toggleRecipeDisplay} style={buttonFour}>Show less</button>
             </div>
             }
            
       </div>
-      </>  
+      
       </div>
+      }
         {requestError &&
           <div style={innerModal}>Oops, something went wrong!
-          <button onClick={this.resetTranscript}>Try Again</button>
+          <button onClick={this.resetTranscript} style={buttonOne}>Try Again</button>
           </div>}
     </div>
     )
@@ -241,29 +251,31 @@ export default App
 
 const styles = {
   main: {
-    // height: '120vh',
+    // height: '200vh',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flexStart',
     alignItems: 'center',
-    color: '#EBE5E4'
+    // backgroundColor: '#EBE5E4',
+    // backgroundSize: 'cover'
   },
   pageWrapper: {
     display: 'flex',
+    height: '100vh',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'flexStart',
     justifyContent: 'center',
     paddingTop: '15px'
   },
   header: {
-    textAlign: 'center',
     fontFamily: 'Carrois Gothic SC, sans-serif',
     fontSize: '50px',
     fontWeight: 'light',
-    color: '#FF5001'
+    color: '#FA7921'
   },
   headerText: {
-    margin: '10px'
+    margin: '10px',
+    padding: '10px',
   },
   underLine: {
     border: 'solid grey 1px',
@@ -271,7 +283,7 @@ const styles = {
   },
   pageWelcome: {
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'flexStart',
     paddingTop: '20px',
     width: '75vw',
     fontFamily: 'Oswald, sans-serif',
@@ -282,7 +294,8 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'flexStart',
+    // backgroundColor: '#EBE5E4'
   },
   buttonOne: {
     width: '120px',
@@ -290,30 +303,46 @@ const styles = {
     background: '#F8F272',
     border: 'grey',
     borderRadius: '50%',
-    margin: '20px',
+    margin: '30px',
     // fontSize: '12px',
     fontWeight: 'bold',
-    boxShadow: '0px 0px 15px 0px'
+    boxShadow: '0px 0px 15px 0px',
+    // padding: '10px',
   },
   buttonTwo: {
     width: '120px',
     height: '120px',
     background: '#94FBAB',
     borderRadius: '50%',
-    margin: '20px',
+    margin: '30px',
     // fontSize: '13px',
     fontWeight: 'bold',
-    boxShadow: '0px 0px 15px 0px'
+    boxShadow: '0px 0px 15px 0px',
+    // padding: '10px',
   },
   buttonThree: {
     width: '120px',
     height: '120px',
     background: '#F4989C',
     borderRadius: '50%',
-    margin: '20px',
-    // fontSize: '13px',
+    margin: '30px',
+    // fontSize: '9px',
     fontWeight: 'bold',
-    boxShadow: '0px 0px 15px 0px'
+    boxShadow: '0px 0px 15px 0px',
+    // padding: '10px',
+  },
+  buttonFour: {
+    width: '80px',
+    height: '30px',
+    background: '#49D49D',
+    color: 'white',
+    borderRadius: '10%',
+    marginTop: '0px',
+    marginRight: '60px',
+    // marginBottom: '20px',
+    fontWeight: 'bold',
+    boxShadow: '0px 0px 15px 0px',
+    // padding: '10px',
   },
   transcriptLabel: {
     color: '#120002',
@@ -328,8 +357,33 @@ const styles = {
     margin: '1em',
     width: '50vw'
   },
+  recipeWrapper: {
+    display: 'flex',
+    padding: '20px',
+    // flexDirection: 'column',
+    justifyContent: 'center',
+    // height: '100vh',
+  },
+  recipeHeader: {
+    textAlign: 'left',
+    fontFamily: 'Carrois Gothic SC, sans-serif',
+    fontSize: '100px',
+    fontWeight: 'heavy',
+    color: '#FA7921',
+    margin: '10px'
+  },
   recipeShow: {
-    width: '75vw'
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '75vw',
+  },
+  recipeList: {
+    display: 'flex',
+    flexDirection: 'column',
+    fontFamily: 'Oswald, sans-serif',
+    fontSize: '15px',
+    color: '#EBE5E4'
   },
   outerModal: {
     display: 'flex',
@@ -341,17 +395,23 @@ const styles = {
   innerModal: {
     display: 'flex',
     flexDirection: 'column',
-    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
     height: '70vh',
     width: '70vw',
-    backgroundColor: 'green',
+    backgroundColor: '#EBE5E4',
     opacity: '1.0',
     border: 'solid grey 1px',
     zIndex: '5',
     position: 'absolute',
     marginTop: '60px',
-    boxShadow: '0px 2px 10px 0px' 
+    boxShadow: '0px 2px 10px 0px',
+    textAlign: 'center',
+    fontFamily: 'Carrois Gothic SC, sans-serif',
+    fontSize: '50px',
+    fontWeight: 'light',
+    color: '#FF5001'
   },
 }
-const {main, pageWrapper, header, headerText, underLine, pageWelcome, pageContent, recipeShow, 
-  inner_elements, buttonOne, buttonTwo, buttonThree, transcriptLabel, final, outerModal, innerModal} = styles
+const {main, pageWrapper, header, headerText, underLine, pageWelcome, pageContent, recipeWrapper, recipeHeader, recipeShow, 
+  recipeList, inner_elements, buttonOne, buttonTwo, buttonThree, buttonFour, transcriptLabel, final, outerModal, innerModal} = styles
